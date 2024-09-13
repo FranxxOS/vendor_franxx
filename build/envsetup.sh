6d47a4f3d4ff0bc937ea9ffb5639eb4837e72c05
hirerokazuoa <hirerokazuoa@franxx.co>
2024-09-13 06:44:16 +0530

    vendor: add more funcs -added from (https://github.com/DerpFest-AOSP/vendor_derp/blob/15/build/envsetup.sh)
    Signed-off-by: hirerokazuoa <hirerokazuoa@franxx.co>

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -1,3 +1,7 @@
+function syncc() {
+    time repo sync --force-sync --no-clone-bundle --current-branch --no-tags "$@"
+}
+
 function brunch()
 {
     breakfast $*
@@ -34,3 +38,38 @@ function breakfast()
     return $?
 }
 
+# Make using all available CPUs
+function mka() {
+    m -j$(nproc --all) "$@"
+}
+
+function cout()
+{
+    if [  "$OUT" ]; then
+        cd $OUT
+    else
+        echo "Couldn't locate out directory.  Try setting OUT."
+    fi
+}
+
+function fixup_common_out_dir() {
+    common_out_dir=$(get_build_var OUT_DIR)/target/common
+    target_device=$(get_build_var TARGET_DEVICE)
+    common_target_out=common-${target_device}
+    if [ ! -z $FRANXX_FIXUP_COMMON_OUT ]; then
+        if [ -d ${common_out_dir} ] && [ ! -L ${common_out_dir} ]; then
+            mv ${common_out_dir} ${common_out_dir}-${target_device}
+            ln -s ${common_target_out} ${common_out_dir}
+        else
+            [ -L ${common_out_dir} ] && rm ${common_out_dir}
+            mkdir -p ${common_out_dir}-${target_device}
+            ln -s ${common_target_out} ${common_out_dir}
+        fi
+    else
+        [ -L ${common_out_dir} ] && rm ${common_out_dir}
+        mkdir -p ${common_out_dir}
+    fi
+}
+
+# Disable ABI checking
+export SKIP_ABI_CHECKS=true
